d7c52ca8db0fc70e5a8497e09a8472a4663cf53f
Einsteinamarjeetk <156225038+Einsteinamarjeetk@users.noreply.github.com>
2024-04-29 18:11:56 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,3 +1,3 @@
-api_id = "21609776"
-api_hash = "61f2558d634df353a2a9806661f4b091"
-bot_token = "6435141772:AAHynl9w9v1om4J_sBsOmIklRI4-aO1UY7Y"
+api_id = "9438917"
+api_hash = "b86192c6db595de8e8863b43648c55da"
+bot_token = "6606738353:AAGLmO0ZG2YaOKy8ni-M4fjjoyAphFGRqjk"
